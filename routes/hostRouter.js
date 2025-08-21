84568691fe92385c0677aadf0a0feeb963a8f6bb
svenoor <svenoor17@gmail.com>
2025-08-20 19:30:11 -0600

    Added cloudinary functionality

--- routes/hostRouter.js
+++ routes/hostRouter.js
@@ -1,15 +1,32 @@
 // External Module
 const express = require("express");
+const multer = require("multer");
+const storage = require("../config/cloudinary");
 const hostRouter = express.Router();
+const upload = multer({ storage });    // << new
 
 // Local Module
 const hostController = require("../controllers/hostController");
 
+// -hostRouter.get("/add-home", hostController.getAddHome);
+// -hostRouter.post("/add-home", hostController.postAddHome);
 hostRouter.get("/add-home", hostController.getAddHome);
-hostRouter.post("/add-home", hostController.postAddHome);
+hostRouter.post(
+  "/add-home",
+  upload.array("images", 8),           // ‘images’ == name of the <input>
+  hostController.postAddHome
+);
+
 hostRouter.get("/host-home-list", hostController.getHostHomes);
 hostRouter.get("/edit-home/:homeId", hostController.getEditHome);
-hostRouter.post("/edit-home", hostController.postEditHome);
+
+// -hostRouter.post("/edit-home", hostController.postEditHome);
+hostRouter.post(
+  "/edit-home",
+  upload.array("images", 8),           // add / replace pics on edit
+  hostController.postEditHome
+);
+
 hostRouter.post("/delete-home/:homeId", hostController.postDeleteHome);
 
 module.exports = hostRouter;
